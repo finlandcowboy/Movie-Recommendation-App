9a2e0a70a07e96ef1c9d6a10aec933ecbec464b5
Zhashkov Stepan <finlandcowboy@gmail.com>
2022-05-26 01:12:21 +0300

    fixed rating model

--- MovieRecommender/models.py
+++ MovieRecommender/models.py
@@ -15,4 +15,7 @@ class Rating(models.Model):
     movie=models.ForeignKey(Movie,on_delete=models.CASCADE,default=None)
     rating=models.CharField(max_length=70)
     rated_date=models.DateTimeField(auto_now_add=True)
-
+    ip_address = models.GenericIPAddressField(default='192.168.0.1')
+    lon = models.CharField(max_length=70, default='0')
+    lat = models.CharField(max_length=70, default='0')
+    city = models.CharField(max_length=70, default='0')
